9f1e8e3cda7a35faacdbfc3fc1bf98afa71fe27e
Peter Lesslie <plesslie@drwholdings.com>
2019-04-05 10:43:21 -0500

    Bucketing

--- src/buckets.cpp
+++ src/buckets.cpp
@@ -1,14 +1,70 @@
 #include <catch2/catch.hpp>
+#include <cstdint>
 #include <iostream>
 #include <vector>
+#include <cassert>
+#include <algorithm>
+#include <climits>
 
-unsigned int Factorial( unsigned int number ) {
-    return number <= 1 ? number : Factorial(number-1)*number;
-}
+struct Bucketeer {
+    Bucketeer(std::vector<uint32_t> bs) : total(0ull), buckets(std::move(bs)), counts(buckets.size() + 1, 0)
+    {
+        buckets.push_back(UINT32_MAX);
+        assert(!buckets.empty() && "invalid buckets configuration");
+    }
 
-TEST_CASE( "Factorials are computed", "[factorial]" ) {
-    REQUIRE( Factorial(1) == 1 );
-    REQUIRE( Factorial(2) == 2 );
-    REQUIRE( Factorial(3) == 6 );
-    REQUIRE( Factorial(10) == 3628800 );
+    void add(uint32_t x) noexcept {
+        size_t bucket = _find_bucket(x);
+        ++counts[bucket];
+        total += x;
+    }
+
+    void del(uint32_t x) noexcept {
+        size_t bucket = _find_bucket(x);
+        --counts[bucket];
+        total -= x;
+    }
+
+    size_t _find_bucket(uint32_t x) const noexcept {
+        assert(x < UINT32_MAX && "invalid value");
+        for (size_t i = 0; ; ++i) {
+            if (x < buckets[i]) {
+                return i;
+            }
+        }
+    }
+
+    uint64_t total;
+    std::vector<uint32_t> buckets;
+    std::vector<uint32_t> counts;
+};
+
+TEST_CASE("Bucketing", "[buckets]") {
+    std::vector<uint32_t> buckets = { 5, 10, 15, 20, 25 };
+
+    Bucketeer bb{buckets};
+    for (size_t i = 0; i < bb.buckets.size(); ++i) {
+        REQUIRE(bb.counts[i] == 0);
+    }
+
+    for (size_t i = 0; i < buckets.size(); ++i) {
+        auto edge = buckets[i];
+        bb.add(edge);
+        REQUIRE(bb.counts[i+1] == 1);
+        REQUIRE(bb.total == edge);
+        bb.del(edge);
+        REQUIRE(bb.counts[i+1] == 0);
+        REQUIRE(bb.total == 0ull);
+    }
+
+    bb.add(3);
+    bb.add(4);
+    bb.add(3);
+    REQUIRE(bb.counts[0] == 3u);
+    REQUIRE(bb.total == 10ull);
+
+    bb.add(5);
+    REQUIRE(bb.counts[0] == 3u);
+    REQUIRE(bb.counts[1] == 1u);
+    REQUIRE(bb.total == 15ull);
 }
